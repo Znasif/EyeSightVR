e9dc0ba4cce2be8293a454d6e7525a5754f42897
znasif <1305030.nz@ugrad.cse.buet.ac.bd>
2021-05-06 16:29:55 -0700

    First Commit
    Signed-off-by: znasif <1305030.nz@ugrad.cse.buet.ac.bd>

---
+++ Source/VREYE/VREYEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VREYEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VREYE_API AVREYEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
