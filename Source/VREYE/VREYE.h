9b8ee7cc3755c7d420a959594d4eb44cc9c1173e
Znasif <1305030.nz@ugrad.cse.buet.ac.bd>
2021-09-02 16:00:42 -0700

    Armaggedon time
    Signed-off-by: Znasif <1305030.nz@ugrad.cse.buet.ac.bd>

--- Source/VREYE/VREYE.h
+++ Source/VREYE/VREYE.h
@@ -3,4 +3,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "VREYE.generated.h"
 
+USTRUCT(BlueprintType)
+struct FScotoma1
+{
+	GENERATED_BODY()
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	FLinearColor MeanColor = FLinearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	float Sigma = 0.0f;
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	float Weight = 0.0f;
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	float Rotation = 0.0f;
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	float Distortion = 0.0f;
+	UPROPERTY(BlueprintReadWrite, Category = "Scotoma")
+	float Tesselation = 0.0f;
+};
